13700b26339c3f4e1a6b7a872c4a3eb5132957fd
drevenb <drevenb@gmail.com>
2018-08-09 08:07:18 +0300

    BE03 Get Objects Name

--- Source/BuildingEscape/PositionReport.h
+++ Source/BuildingEscape/PositionReport.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
 #include "PositionReport.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
