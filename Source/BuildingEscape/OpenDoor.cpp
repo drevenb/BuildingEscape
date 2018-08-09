7558ceae2b0151377dfc24be9633b7c3fb9005b6
drevenb <drevenb@gmail.com>
2018-08-09 16:06:38 +0300

    BE05 New Script OpenDoor

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// Treant Studio 2018
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the owning actor
+	AActor* Owner = GetOwner();
+	//Create a rotater
+	FRotator NewRotation = FRotator(0.f, -90.f, 0.f);
+	//Set the door rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
