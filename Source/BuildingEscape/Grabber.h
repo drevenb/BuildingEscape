a8fce857a7e7f513bd174f2444c24cdde92390d9
drevenb <drevenb@gmail.com>
2018-08-23 09:21:55 +0300

    BE12 Grab objects and release them

---
+++ Source/BuildingEscape/Grabber.h
@@ -0,0 +1,54 @@
+// Treant Studio 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+#include "DrawDebugHelpers.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	//How far ahead of the player can we reach in cm
+		float Reach = 100.f;
+		
+		UPhysicsHandleComponent* PhysicsHandle = nullptr;
+		UInputComponent* InputComponent = nullptr;
+
+		///Ray-cast and grab what's is reach
+		void Grab();
+
+		///Called when grab is released
+		void Release();
+
+		//Setup (assumed) attached input component
+		void SetupInputComponent();
+
+		//Find (assumed) attached physics handle
+		void FindPhysicsHandleComponent();
+
+		//Return hit for first physics body in reach
+		FHitResult GetFirstPhysicsBodyInReach() const; 
+};
